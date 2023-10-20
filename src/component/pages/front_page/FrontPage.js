de489043a6b19df71389fe7e2c5b017851a12cfb
Eshan DeSilva <hasindueshandesilva@gmail.com>
2023-10-20 11:30:27 +0530

    front page completed

--- src/component/pages/front_page/FrontPage.js
+++ src/component/pages/front_page/FrontPage.js
@@ -1,6 +1,8 @@
 import React from "react";
 import "../../styles/style.css";
 import image from "../../../resources/img/favicon.png";
+import { AwesomeButton } from "react-awesome-button";
+import 'react-awesome-button/dist/styles.css';
 
 export default function FrontPage() {
   return (
@@ -11,10 +13,24 @@ export default function FrontPage() {
           <div className="col-sm-12 col-md-8 frnt-txt">
             <div className="frnt-hdn-txt">
               Hello, it's me
-              <div className="frnt-hdn-nm">Hasindu Eshan</div>
+              <div className="frnt-hdn-nm">
+                Hasindu <span className="sub-txt">Eshan</span>
+              </div>
               <div className="frnt-hdn-desc">
-                A Passionate Full Stack Developer about creating interactive
-                front-end and back-end applications and experience on web.
+                As a passionate full-stack developer, I specialize in crafting
+                engaging front-end and back-end applications, backed by years of
+                hands-on experience in web development.
+              </div>
+              <div>
+                <AwesomeButton
+                  type="secondary"
+                  className="aws-btn"
+                  onPress={() => {
+                    console.log("pressed");
+                  }}
+                >
+                  More About..
+                </AwesomeButton>
               </div>
             </div>
           </div>
