91fa07f5c16e05c8eb7a995a89adf719a312ae97
Eshan DeSilva <hasindueshandesilva@gmail.com>
2023-10-20 16:01:26 +0530

    front page completed with responsive

--- src/component/pages/front_page/FrontPage.js
+++ src/component/pages/front_page/FrontPage.js
@@ -10,23 +10,24 @@ export default function FrontPage() {
     <div className="frnt-pg">
       <div className="container-fluid">
         <div className="row">
-          <img src={image} alt="user" className="col-sm-12 col-md-4" />
+          <div className="col-sm-12 col-md-4 dp">
+            <img src={image} alt="user" className="col-sm-12 col-md-4" style={{width:"100%",marginTop:"100px"}}/>
+          </div>
 
           <div className="col-sm-12 col-md-8 frnt-txt">
             <div className="frnt-hdn-txt">
+              <div className="frnt-grt">
               Hello, it's me
-              
-              <div className="frnt-hdn-nm">
-                Hasindu <span className="sub-txt">Eshan</span>
               </div>
-
+              <div className="frnt-hdn-nm">
+                Hasindu<span className="sub-txt">&nbsp;Eshan</span>
+              </div>
               <div className="frnt-hdn-desc">
                 As a passionate full-stack developer, I specialize in crafting
                 engaging front-end and back-end applications, backed by years of
                 hands-on experience in web development.
               </div>
-
-              <div>
+              <div className="frnt-btn">
                 <AwesomeButton
                   type="secondary"
                   className="aws-btn"
@@ -37,7 +38,6 @@ export default function FrontPage() {
                   More About..
                 </AwesomeButton>
               </div>
-
               <div className="em-div">
                 <img src={gmImg} alt="gmail" style={{ width: "50px" }} />
                 <div className="em-txt">hasindueshandesilva@gmail.com</div>
